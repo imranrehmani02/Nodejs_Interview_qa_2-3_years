7ccec13efd4ae847c2ba66aee55d4296bcc6c02f
imranrehmani02 <mo.imranrehmani02@gmail.com>
2023-09-08 12:52:18 +0530

    day 1 of API

---
+++ RestAPI/microservice.js
@@ -0,0 +1,40 @@
+/*
+1. What are microservices ?
+Ans. In micro service architecture each application is composed of many smaller,
+    loosly coupled and independently deployable services.
+
+    -> Microservices are a software architecture pattern in which complex applications
+    are decomposed into small, independent and loosly coupled services.
+            Each service is built around a specific business capabilities and can be 
+    deployed, developed and scaled independently. These services communicate with each 
+    other through well defined API's, typically over a network.
+
+
+
+2. How does microservices work ?
+Ans. Microservice architecture works by decomposing a complex application into smaller,
+    independent services that can be developed, deployed and scaled independently.
+    These services communicate with each other through well-defined API's or protocols.
+
+
+
+3. How to connect multiple projects in one microservice ?
+Ans. Connecting multiple projects in a microservice architecture typicaly involves
+    establishing communication channels between the services.
+    1) Restful API
+    2) Messaging systems
+    3) Event driven architecture
+    4) API gateways
+    5) Shared databases
+
+
+
+4. Difference between monolithic and microservice based architecture ?
+Ans. Monolithic:- Monolithic architecture is built as one large system and is usually
+                one code base. It is not easy to scale based on demand. It has shared
+                database.
+
+    Microservices:- Microservices architecture is built as small independent module
+                based on business functionality. It is easy to scale based on demand.
+                Each project and module has their own database 
+*/
