f6fcdc93d85d56479739c2865bc596e48e505eca
imranrehmani02 <mo.imranrehmani02@gmail.com>
2023-09-19 18:36:38 +0530

    sql changes

---
+++ MongoDB/mongo2.js
@@ -0,0 +1,32 @@
+/*
+Q. What is MongoDB shell (mongosh) ?
+Ans. The MongoDB Shell, mongosh, is a fully functional JavaScript and Node.js 
+    REPL environment for interacting with MongoDB deployments. You can use the
+    MongoDB Shell to test queries and operations directly with your database.
+
+
+
+Q. What is database sharding ?
+Ans. Database sharding is the process of storing a large database across multiple 
+    machines. A single machine, or database server, can store and process only a 
+    limited amount of data. Database sharding overcomes this limitation by splitting 
+    data into smaller chunks, called shards, and storing them across several database servers.
+        All database servers usually have the same underlying technologies, and they 
+    work together to store and process large volumes of data.
+
+
+
+Q. What is sharding in mongodb ?
+Ans. Sharding is a method for distributing data across multiple machines. MongoDB uses 
+    sharding to support deployments with very large data sets and high throughput operations.
+                Database systems with large data sets or high throughput applications 
+    can challenge the capacity of a single server.
+
+
+
+Q. What is Mongodb charts ?
+Ans. MongoDB Charts is a tool to create visual representations of your MongoDB data. 
+                Data visualization is a key component to providing a clear understanding
+    of your data, highlighting correlations between variables and making it easy to
+    discern patterns and trends within your dataset. 
+*/
